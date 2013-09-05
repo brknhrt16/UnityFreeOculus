0d2d5e563bc2ff3f5ad4d9c8b686f34bc28c910f
mschrod <mschrod@umich.edu>
2013-09-05 14:34:11 -0400

    Fixed errors in Gui3DText

--- Assets/Gui3D/Gui3DText.cs
+++ Assets/Gui3D/Gui3DText.cs
@@ -7,28 +7,33 @@ namespace Gui3D
 	{
 		public Gui3DText (Rect boundingBox, float depth, string text)
 		{
+			gameObject = new GameObject("TextField");
+			gameObject.AddComponent<TextMesh>();
+			gameObject.AddComponent<MeshRenderer>();
+			MeshRenderer meshRender = gameObject.GetComponent<MeshRenderer>();
+			meshRender.material = (Material)Resources.GetBuiltinResource(typeof(Material), "Arial.ttf");
+			gameObject.GetComponent<TextMesh>().text = text;
+			Font font = (Font)Resources.GetBuiltinResource(typeof(Font), "Arial.ttf");
+			gameObject.GetComponent<TextMesh>().font = font;
+			gameObject.transform.localPosition = new Vector3(boundingBox.x, boundingBox.y, depth);
+			gameObject.transform.localScale = new Vector3(boundingBox.width, boundingBox.height, 1);
 			this.boundingBox = boundingBox;
 			this.depth = depth;
-			gameObject = new TextMesh();
-			TextMesh textMesh = gameObject as TextMesh;
-			textMesh.guiText = text;
-			textMesh.transform.localPosition = new Vector3(boundingBox.x, boundingBox.y, depth);
-			textMesh.transform.localScale = new Vector3(boundingBox.width, boundingBox.height, 1);
 		}
 		
-		Rect boundingBox
+		public Rect boundingBox
 		{
-			get;
+			get; //TODO: update gameObject
 			set;
 		}
 		
-		float depth
+		public float depth
 		{
-			get;
+			get; //TODO: update gameObject
 			set;
 		}
 		
-		GameObject gameObject
+		public GameObject gameObject
 		{
 			private set;
 			get;
