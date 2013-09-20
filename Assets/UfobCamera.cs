58ed851db00e91947bfb5d09f6c460871b107801
Khevna Shah <khevnashah@gmail.com>
2013-09-20 18:28:55 -0400

    Added stuff to the UfobCamera script to switch modes

---
+++ Assets/UfobCamera.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UfobCamera : MonoBehaviour 
+{
+	private bool HmdMode = true;
+	public bool OverrideToHmdMode = false;
+	public GameObject [] HmdObjects;
+	public GameObject [] NonHmdObjects;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		if(!OverrideToHmdMode)
+		{
+			GetHmdMode();
+		}
+		if(HmdObjects.Length == 0)
+		{
+			GetHmdObjects();
+		}
+		if(NonHmdObjects.Length == 0)
+		{
+			GetNonHmdObjects();
+		}
+		if(HmdMode)
+		{
+			Disable(NonHmdObjects);
+			Enable(HmdObjects);
+		}
+		else
+		{
+			Enable(NonHmdObjects);
+			Disable(HmdObjects);
+		}
+	}
+	
+	void GetHmdMode()
+	{
+		HmdMode = false;
+		string [] args = System.Environment.GetCommandLineArgs();
+		foreach(string arg in args)
+		{
+			if(arg == "-hmd")
+			{
+				HmdMode = true;
+			}
+		}
+	}
+	
+	void GetHmdObjects()
+	{
+		HmdObjects = new GameObject[1];
+		HmdObjects[0] = transform.Find("HmdCamera").gameObject;
+	}
+	
+	void GetNonHmdObjects()
+	{
+		NonHmdObjects = new GameObject[1];
+		NonHmdObjects[0] = transform.Find("NonHmdCamera").gameObject;
+	}
+	
+	void Disable(GameObject [] objects)
+	{
+		foreach(GameObject o in objects)
+		{
+			o.SetActive(false);
+		}
+	}
+	
+	void Enable(GameObject [] objects)
+	{
+		foreach(GameObject o in objects)
+		{
+			o.SetActive(true);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+	
+	
+}
