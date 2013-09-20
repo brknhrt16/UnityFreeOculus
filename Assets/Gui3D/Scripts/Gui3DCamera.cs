b676be7e3e22c226da7358092dcadad7cab8e79d
mschrod <mschrod@umich.edu>
2013-09-20 18:28:15 -0400

    Updated guibutton and guicameras

---
+++ Assets/Gui3D/Scripts/Gui3DCamera.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gui3DCamera : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.camera.transparencySortMode = TransparencySortMode.Orthographic;
+	}
+}
