2f13b709ddd015929089f538c3d1ef10eae0df8d
Khevna Shah <khevnashah@gmail.com>
2013-09-28 01:20:45 -0400

    Basic Menu

---
+++ Assets/Gui3D/Scripts/Gui3DMenu.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Gui3D
+{
+	public class Gui3DMenu : MonoBehaviour 
+	{
+		public bool UseMenuIndex = false;
+		public List<GameObject> MenuObjects;
+		
+		// Use this for initialization
+		void Start () 
+		{
+			if(UseMenuIndex)
+			{
+				FillMenuByIndex();
+			}
+			else if(MenuObjects.Count <= 0)
+			{
+				FillMenuAutomatically();
+			}
+		}
+		
+		// Update is called once per frame
+		void Update () 
+		{
+			
+		}
+		
+		void FillMenuByIndex()
+		{
+			FillMenuAutomatically();
+			MenuObjects.Sort(Gui3DMenuUtils.IndexCompare);
+		}
+		
+		void FillMenuAutomatically()
+		{
+			MenuObjects = new List<GameObject>();
+			foreach(Transform child in transform)
+			{
+				if(child.gameObject.GetComponent<Gui3DObject>() != null)
+				{
+					if((child.gameObject.GetComponent<Gui3DObject>()).Selectable == true)
+					{
+						MenuObjects.Add(child.gameObject);
+					}
+				}
+			}
+			MenuObjects.Sort(Gui3DMenuUtils.LocationCompare);
+		}
+	}
+}
