08e2d1f401112c8542ccb0135491ede9c18a060a
mschrod <mschrod@umich.edu>
2013-09-28 02:51:00 -0400

    Added cursor prefab and Added HoverObject to Gui3D

--- Assets/Gui3D/Scripts/Gui3D.cs
+++ Assets/Gui3D/Scripts/Gui3D.cs
@@ -8,5 +8,29 @@ namespace Gui3D
 	{
 	    public GameObject Cursor;
 		public UfobCamera GuiCamera;
+		
+		public Gui3DObject HoverObject
+		{
+			private set;
+			get;
+		}
+		
+		void Update()
+		{
+			// Ray
+			Ray ray = new Ray(GuiCamera.gameObject.transform.position, Cursor.transform.position);
+			
+			// Raycast Hit
+			RaycastHit hit;
+			
+			if (Physics.Raycast(ray, out hit, 1000))
+			{
+				HoverObject = hit.transform.gameObject.GetComponent<Gui3DObject>();
+			}
+			else
+			{
+				HoverObject = null;
+			}
+		}
 	}
 }
