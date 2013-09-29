b2a2eff197edb67149b6deb7fc99a4cb25215d78
Khevna Shah <khevnashah@gmail.com>
2013-09-29 00:41:55 -0400

    Updated Gui3DObject to remove function and moved it to Gui3DUtils

---
+++ Assets/Gui3D/Scripts/Gui3DUtils.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Gui3D
+{
+	public class Gui3DUtils : MonoBehaviour 
+	{
+		public static Gui3D GetNearestGui3D(GameObject obj)
+		{
+			Gui3D Gui = obj.transform.root.GetComponent<Gui3D>();
+			if(Gui == null)
+			{
+				Gui = obj.transform.root.GetComponentInChildren<Gui3D>();
+			}
+			if(Gui == null)
+			{
+				Debug.LogWarning("Gui3D not a parent of " + obj.transform.gameObject.name);
+				GameObject.Find("Gui3D").GetComponent<Gui3D>();
+			}
+			
+			if(Gui == null)
+			{
+				Debug.LogError("Could not find a Gui3D");
+			}
+			
+			return Gui;
+		}
+	}
+}
