1d041b308efc297f5a7f0faa390d7b8ebc4ec7c5
mschrod <mschrod@umich.edu>
2013-09-30 02:26:38 -0400

    Toggle mouse no longer relies on Screen.cursorLocked

--- Assets/Gui3D/Scripts/ButtonFunctions/ToggleMouse.cs
+++ Assets/Gui3D/Scripts/ButtonFunctions/ToggleMouse.cs
@@ -6,10 +6,13 @@ namespace Gui3D
 	
 	public class ToggleMouse : MonoBehaviour 
 	{
-		
 		public bool ToggleState = true;
 		public bool SetToLocked = true;
 		public bool HideCursor = true;
+		
+		public bool SetLocked = true;
+		
+		public bool SetMoveWithMouse = true;
 		
 		public bool SetCursorPosition = false;
 		public Vector3 CursorPosition = new Vector3(0, 0, 1000);
@@ -17,6 +20,7 @@ namespace Gui3D
 		// Use this for initialization
 		void Start () 
 		{
+			Screen.lockCursor = true;
 			gameObject.GetComponent<Gui3DButton>().OnPush += new Gui3DButton.OnPushEvent(Toggle);
 		}
 		
@@ -28,25 +32,50 @@ namespace Gui3D
 		
 		void Toggle()
 		{
-			bool newState = !Screen.lockCursor;
-			if (!ToggleState)
+			bool newState = SetToLocked;
+			if (SetLocked)
 			{
-				newState = SetToLocked;	
+				if (ToggleState)
+				{
+					Screen.lockCursor = newState;
+				}
+				else
+				{
+					Screen.lockCursor = !Screen.lockCursor;
+				}
 			}
-			Screen.lockCursor = newState;
 			GameObject cursorObject = gameObject.GetComponent<Gui3DObject>().GetGui3D().Cursor;
-			if (cursorObject != null && cursorObject.GetComponent<Gui3DCursor>() != null)
+			if (SetMoveWithMouse && cursorObject != null && cursorObject.GetComponent<Gui3DCursor>() != null)
 			{
-				cursorObject.GetComponent<Gui3DCursor>().MoveCursorWithMouse = !newState;
+				if (ToggleState)
+				{
+					cursorObject.GetComponent<Gui3DCursor>().MoveCursorWithMouse = !cursorObject.GetComponent<Gui3DCursor>().MoveCursorWithMouse;
+				}
+				else
+				{
+					cursorObject.GetComponent<Gui3DCursor>().MoveCursorWithMouse = !newState;
+				}
 			}
 			if (HideCursor && cursorObject != null && cursorObject.GetComponent<DrawToScreen>() != null)
 			{
-				cursorObject.GetComponent<DrawToScreen>().enabled = !newState;
+				if (ToggleState)
+				{
+					cursorObject.GetComponent<DrawToScreen>().enabled = !cursorObject.GetComponent<DrawToScreen>().enabled;
+				}
+				else
+				{
+					cursorObject.GetComponent<DrawToScreen>().enabled = !newState;
+				}
 			}
 			if (SetCursorPosition && cursorObject != null)
 			{
 				cursorObject.transform.position = CursorPosition;
 			}
+		}
+		
+		void ToggleMoveWithMouse()
+		{
+			
 		}
 	}
 }
