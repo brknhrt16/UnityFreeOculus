99893a8461db4ed165ad1cb874fc204946ab30ca
Khevna Shah <khevnashah@gmail.com>
2013-10-07 01:25:10 -0400

    Added check box whoo!

---
+++ Assets/Gui3D/Scripts/Gui3DCheckBox.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Gui3D
+{
+	public class Gui3DCheckBox : Gui3DButton 
+	{
+		public bool Checked = false;
+		
+		void Start()
+		{
+			OnPush += new OnPushEvent(OnCheck);
+		}
+		
+		void OnCheck()
+		{
+			Checked = !Checked;
+		}
+	}
+}
---
+++ Assets/Gui3D/Scripts/SelectionFunctions/ChangeMaterialOnCheck.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Reflection;
+
+namespace Gui3D
+{
+	public class ChangeMaterialOnCheck : MonoBehaviour 
+	{
+	
+		public GameObject Obj = null;
+		public Material SelectedMaterial = null;
+		public Material DeselectedMaterial = null;
+		
+		private bool Checked = false;
+			
+		// Use this for initialization
+		void Start () 
+		{
+			MeshRenderer mesh = Obj.GetComponent<MeshRenderer>();
+			if(mesh != null)
+			{
+				if(gameObject.GetComponent<Gui3DCheckBox>().Checked)
+				{
+					mesh.material = SelectedMaterial;
+					Checked = true;
+				}
+				else
+				{
+					Checked = false;
+					mesh.material = DeselectedMaterial;
+				}
+				
+			}
+			gameObject.GetComponent<Gui3DCheckBox>().OnPush += new Gui3DButton.OnPushEvent(ToggleMaterial);
+		}
+		
+		
+		void ToggleMaterial()
+		{
+			Checked = !Checked;
+			if((Obj != null) && (SelectedMaterial != null) && (DeselectedMaterial != null))
+			{				
+				MeshRenderer mesh = Obj.GetComponent<MeshRenderer>();
+				if(mesh != null)
+				{
+					if(Checked)
+					{
+						mesh.material = SelectedMaterial;
+					}
+					else
+					{
+						mesh.material = DeselectedMaterial;
+					}
+				}
+			}
+		}
+	}
+}
