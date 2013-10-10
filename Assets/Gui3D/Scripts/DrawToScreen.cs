cc6a9193ca96761e80f1c4a0a28cf244c0a12ef1
Khevna Shah <khevnashah@gmail.com>
2013-10-10 00:48:22 -0400

    Fixed bug for the million time. only draw to cam when enabled

--- Assets/Gui3D/Scripts/DrawToScreen.cs
+++ Assets/Gui3D/Scripts/DrawToScreen.cs
@@ -38,35 +38,38 @@ namespace Gui3D
 			Gui3DObject guiObject = gameObject.GetComponent<Gui3DObject>();
 			foreach (Camera camera in guiObject.GetGui3D().GuiCameras)
 			{
-				Vector3 screenPosition = camera.WorldToScreenPoint(gameObject.transform.position);
-				
-				Rect imageRect = new Rect(screenPosition.x + Offset.x, camera.pixelHeight - screenPosition.y + Offset.y, Size.x, Size.y);
-				Rect clampedRect = imageRect;
-				if (camera.pixelRect.xMin > clampedRect.xMin)
+				if(camera.enabled)
 				{
-					clampedRect.xMin = camera.pixelRect.xMin;
-				}
-				if (camera.pixelRect.yMin > clampedRect.yMin)
-				{
-					clampedRect.yMin = camera.pixelRect.yMin;
-				}
-				if (camera.pixelRect.xMax < clampedRect.xMax)
-				{
-					clampedRect.xMax = camera.pixelRect.xMax;
-				}
-				if (camera.pixelRect.yMax < clampedRect.yMax)
-				{
-					clampedRect.yMax = camera.pixelRect.yMax;
-				}
-				Rect texCoordRect = new Rect(
-					(clampedRect.xMin - imageRect.xMin) / imageRect.width,
-					(imageRect.yMax - clampedRect.yMax) / imageRect.height,
-					clampedRect.width / imageRect.width,
-					clampedRect.height / imageRect.height);
-				
-				if (clampedRect.width > 0 && clampedRect.height > 0)
-				{
-					GUI.DrawTextureWithTexCoords(clampedRect, Image, texCoordRect);
+					Vector3 screenPosition = camera.WorldToScreenPoint(gameObject.transform.position);
+					
+					Rect imageRect = new Rect(screenPosition.x + Offset.x, camera.pixelHeight - screenPosition.y + Offset.y, Size.x, Size.y);
+					Rect clampedRect = imageRect;
+					if (camera.pixelRect.xMin > clampedRect.xMin)
+					{
+						clampedRect.xMin = camera.pixelRect.xMin;
+					}
+					if (camera.pixelRect.yMin > clampedRect.yMin)
+					{
+						clampedRect.yMin = camera.pixelRect.yMin;
+					}
+					if (camera.pixelRect.xMax < clampedRect.xMax)
+					{
+						clampedRect.xMax = camera.pixelRect.xMax;
+					}
+					if (camera.pixelRect.yMax < clampedRect.yMax)
+					{
+						clampedRect.yMax = camera.pixelRect.yMax;
+					}
+					Rect texCoordRect = new Rect(
+						(clampedRect.xMin - imageRect.xMin) / imageRect.width,
+						(imageRect.yMax - clampedRect.yMax) / imageRect.height,
+						clampedRect.width / imageRect.width,
+						clampedRect.height / imageRect.height);
+					
+					if (clampedRect.width > 0 && clampedRect.height > 0)
+					{
+						GUI.DrawTextureWithTexCoords(clampedRect, Image, texCoordRect);
+					}
 				}
 			}
 		}
